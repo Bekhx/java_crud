0d227c005af06b13bce6571c57591e75116e31b6
bekhx <bekxhm@gmail.com>
2022-12-15 11:47:14 +0500

    Created pages for creating product and products list.

---
+++ front/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/Products.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/products",
+      name: "products",
+      component: HomeView,
+    },
+    {
+      path: "/products/create",
+      name: "create",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/CreateProduct.vue"),
+    },
+  ],
+});
+
+export default router;
---
+++ front/src/views/CreateProduct.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="create_product">
+    <form>
+      <div class="form-group">
+        <label for="product_name">Name</label>
+        <input
+          type="email"
+          class="form-control name_input"
+          id="product_name"
+          aria-describedby="emailHelp"
+          placeholder="Enter name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="product_price">Price</label>
+        <input
+          type="number"
+          required
+          step=".01"
+          class="form-control price_input"
+          id="product_price"
+          placeholder="0.00"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary submit">Create</button>
+    </form>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .create_product {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+
+  .submit {
+    margin-top: 15px;
+  }
+
+  #product_price,
+  #product_name {
+    background-color: #444;
+  }
+
+  .price_input,
+  .name_input {
+    border-color: #333;
+  }
+}
+</style>
